4c1d72a570d537f0361c65dd07672b5a04aafa70
Rubens Pinheiro <torettodev@gmail.com>
2023-04-28 14:01:39 -0300

    header + navbar

--- componentes/layout/Cabecalho.js
+++ componentes/layout/Cabecalho.js
@@ -2,9 +2,53 @@ import Image from "next/legacy/image";
 import logoHorizontalImg from "../../public/imagens/logoHorizontal.svg";
 import imagemLupa from "../../public/imagens/lupa.svg";
 import Navegacao from "./Navegacao";
+import { useState } from "react";
+import ResultadoPesquisa from "./ResultadoPesquisa";
 
 
 export default function Cabecalho( ) {
+    const [resultadoPesquisa, setResultadoPesquisa] = useState([]);
+    const [termoPesquisado, setTermoPesquisado] = useState([]);
+
+    const aoPesquisasr = (e) => {
+        setTermoPesquisado(e.target.value);
+        setResultadoPesquisa([]);
+
+        if (termoPesquisado.length <3) {
+            return;
+        }
+
+        setResultadoPesquisa([
+            {
+                avatar: '',
+                nome: 'toretto',
+                email: 'toretto@toretto.com',
+                _id : '12345'
+
+            },
+
+            {
+                avatar: '',
+                nome: 'Toretta',
+                email: 'toretta@toretto.com',
+                _id : '2655565',
+
+            },
+
+            {
+                avatar: '',
+                nome: 'torettao',
+                email: 'torettao@toretto.com',
+                _id : '18888885',
+
+            }
+        ])
+    }
+
+    const aoClicarResultadoPesquisa = () => {
+        console.log('aoClicarResultadoPesquisa', {id});
+    }
+
     return (
         <header className="cabecalhoPrincipal">
             <div className="conteudoCabecalhoPrincipal">
@@ -27,15 +71,31 @@ export default function Cabecalho( ) {
                 <input
                     type="text"    
                     placeholder="Pesquisar"
-                    value={''}
-                    onChange={() => console.log ("pesquisando")}
+                    value={termoPesquisado}
+                    onChange={aoPesquisasr}
                 />    
                 </div>
 
                 <Navegacao className="desktop" />
             </div>
 
+            {resultadoPesquisa.length > 0 && (
+                <div className="resultadoPesquisaContainer">
+                    {resultadoPesquisa.map(r => (
+                        <ResultadoPesquisa
+                            avatar= {r.avatar}
+                            nome= {r.nome}
+                            email= {r.email}
+                            key={r._id}
+                            id={r._id}
+                            onClick={aoClicarResultadoPesquisa}
+                        />
+                     ))}
+
+            </div>
+            )};
+
+
         </header>
-        
     );
 }
---
+++ componentes/layout/ResultadoPesquisa.js
@@ -0,0 +1,14 @@
+import Avatar from "../avatar";
+
+export default function ResultadoPesquisa ({ nome, avatar, email, onClick , id }) {
+    return (
+        <div className="resultadoPesquisa"onClick={() => onclick(id)}>
+            <Avatar src = {avatar} />
+            <div className="informacoesUsuario">
+                <strong>{nome}</strong>
+                <span>{email}</span>
+            </div>
+
+        </div>
+    );
+}
